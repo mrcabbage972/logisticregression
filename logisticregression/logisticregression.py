61afd5249478847421a2c3cf780dbcd483f7ced4
mrcabbage972 <mayvic@gmail.com>
2015-11-28 00:41:22 +0200

    Adding

---
+++ logisticregression/logisticregression.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+from optimization import gradient_descent_backtrack
+
+
+def p(b, x):
+    return 1.0 / (1.0 + np.exp(-np.dot(b, x)))
+
+
+def lr_loss_gradient(X, y, b, reg_param = 0):
+    grad = (np.dot(np.transpose((p(X, np.transpose(b)))), X) - np.dot(np.transpose(X), y))[0, :]
+    grad += 2.0 * reg_param * b[0, :]
+    return grad
+
+
+def lr_loss(X, y, b, reg_param = 0):
+    loss = -np.sum(np.multiply(np.log(p(b, np.transpose(X))),  np.transpose(y))
+                 + np.multiply(np.log(1.0 - p(b, np.transpose(X))), 1.0 -  np.transpose(y)))
+
+    reg = reg_param * np.dot(b, np.transpose(b))
+    loss += reg[0]
+    return loss[0]
+
+class LogisticRegression:
+    def __init__(self, reg_param = 0.0):
+
+        self.reg_param = reg_param
+
+    def fit(self, X, y):
+
+        opt_loss_func = lambda b : lr_loss(X, y, b, self.reg_param)
+        opt_grad_loss_func = lambda b : lr_loss_gradient(X, y, b, self.reg_param)
+
+        self.coef = gradient_descent_backtrack(opt_loss_func, opt_grad_loss_func, np.zeros((1, X.shape[1])))
+
+    def predict(self, X):
+        return p(self.coef, X)
+
+
+
---
+++ logisticregression/logisticregression_test.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 20 19:23:03 2015
+
+@author: Vic
+"""
+import sklearn.linear_model
+
+from logisticregression import *
+from sklearn import datasets
+'''
+m = 5
+n = 6
+X = np.zeros((n,m))
+#X[:, 0] = 1.0 * np.ones((5))
+#X[:, 1] = -1.0 * np.ones((5))
+X[:, 0] = 0.5
+X[2:5, 1] = 0.1
+y = np.ones((n,1))
+y[0:2, :] = 0
+b0 = np.zeros((1, m))
+'''
+iris = datasets.load_iris()
+X = iris.data[:100, :3]  # we only take the first two features.
+y = np.transpose(iris.target[:100])
+
+reg_param = 0.5
+
+
+#b = gradient_descent_numeric(opt_func, b0)
+#print 'numeric b={}'.format(b)
+
+print 'analytic b={}'.format(b)
+
+LR = sklearn.linear_model.LogisticRegression(C=1,  fit_intercept=False)
+LR.fit(X, y)
+LR_p = LR.predict_proba(X)
+
+print 'sklearn coef={}'.format(LR.coef_)
+
+LR_p = LR.predict_proba(X)
+'''
+for i in xrange(0, len(y)):
+    pred = p(b, X[i, :])
+
+    print '{}, {}, {}'.format(y[i], pred, LR_p[i, 1])'''
