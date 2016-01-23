110cca72623b221ceeabe41c601f7c542aa604fa
mrcabbage972 <mayvic@gmail.com>
2016-01-23 22:11:54 +0200

    Adding test.

--- tests/test_logisticregression.py
+++ tests/test_logisticregression.py
@@ -5,8 +5,8 @@ import logisticregression
 np.random.seed(seed=1)
 
 def make_test_dataset():
-    num_obs = 5000
-    params = [0.1, 0.2, 0.01]
+    num_obs = 100000
+    params = [0.1, -0.2, 0.3]
 
     X = np.ndarray(shape=(num_obs, len(params)))
     y = np.ndarray(shape=(num_obs))
@@ -16,15 +16,19 @@ def make_test_dataset():
         proba = logisticregression.sigmoid(params, x)
         y[i] = np.random.binomial(1, proba , 1)
         X[i, :] = x
-    return X, y
+    return X, y, params
 
 
 class TestLogisticRegression(unittest.TestCase):
-    def test_fixed_step_size(self):
-        X, y = make_test_dataset()
+    def test_auto_step_size(self):
+        X, y, true_params = make_test_dataset()
 
-        lr_fixed_step = logisticregression.LogisticRegression(reg_param=0.00, store_iter_loss=False, step_size=0.001)
-        lr_fixed_step.fit(X, y)
+        lr = logisticregression.LogisticRegression(reg_param=0.00, store_iter_loss=False, step_size='auto',
+                                                              is_verbose=False)
+        lr.fit(X, y)
+
+        rel_error = np.abs((lr.coef - true_params) / true_params)
+        self.assertTrue(np.max(rel_error) < 0.1)
 
 
 if __name__ == '__main__':
